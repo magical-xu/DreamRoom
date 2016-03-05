b7f48983712efa6ebb878e4d1844e93e3435ae15
magical-xu <zx18507008756@163.com>
2016-03-05 15:27:59 +0800

    首次提交

---
+++ app/src/main/java/com/idreamsky/dreamroom/base/BaseActivity.java
@@ -0,0 +1,102 @@
+package com.idreamsky.dreamroom.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.idreamsky.dreamroom.R;
+
+import org.xutils.x;
+
+/**
+ * Created by Administrator on 2016/3/5.
+ */
+public class BaseActivity extends AppCompatActivity{
+
+    private FragmentManager fragmentManager;
+    private Fragment showFragment;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        x.view().inject(this);//把当前的activity注册进xutils3.0
+        fragmentManager = getSupportFragmentManager();
+
+        init();
+        loadDatas();
+    }
+
+    /**
+     * 初始化
+     */
+    public void init(){
+
+    }
+
+    /**
+     * 加载数据
+     */
+    public void loadDatas(){
+
+    }
+
+    /**
+     * 带默认过程动画的启动Activity方式
+     * @param intent
+     */
+    @Override
+    public void startActivity(Intent intent) {
+        this.startActivity(intent, R.anim.activity_in_rigth, R.anim.activity_out);
+    }
+
+    /**
+     * 自定义过场动画的Activity启动方式
+     * @param intent
+     * @param animin
+     * @param animout
+     */
+    public void startActivity(Intent intent, int animin, int animout) {
+        super.startActivity(intent);
+        overridePendingTransition(animin, animout);
+    }
+
+    /**
+     * fragment管理 -- 不包含过程动画的方式
+     */
+    public void fragmentManager(int fl_resid, Class fclass){
+        fragmentManager(fl_resid, 0, 0, fclass);
+    }
+
+    /**
+     * 包含动画的fragment管理
+     * @param fl_resid 给fragment占位的布局id
+     * @param fclass fragment进入的动画
+     * @param inanim fragment退出的动画
+     * @param outanim 需要显示的fragment的Class对象
+     */
+    public void fragmentManager(int fl_resid, int inanim, int outanim, Class<BaseFragment> fclass){
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(inanim != 0 || outanim != 0){
+            fragmentTransaction.setCustomAnimations(inanim, outanim);
+        }
+
+        Fragment fragmentByTag = fragmentManager.findFragmentByTag(fclass.getName());
+        if(showFragment != null){
+            fragmentTransaction.hide(showFragment);
+        }
+        if(fragmentByTag != null){
+            fragmentTransaction.show(fragmentByTag);
+            showFragment = fragmentByTag;
+        } else {
+            BaseFragment baseFragment = BaseFragment.getInstance(fclass);
+            fragmentTransaction.add(fl_resid, baseFragment, fclass.getName());
+            showFragment = baseFragment;
+        }
+        fragmentTransaction.commit();
+    }
+
+
+}
---
+++ app/src/main/java/com/idreamsky/dreamroom/base/BaseFragment.java
@@ -0,0 +1,142 @@
+package com.idreamsky.dreamroom.base;
+
+/**
+ * Created by Administrator on 2016/3/5.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.xutils.x;
+
+import java.io.Serializable;
+
+/**
+ * Fragement基类 -- 泛型K 为需要传递到该fragment中的类型
+ */
+public class BaseFragment<K extends Serializable> extends Fragment {
+    private boolean injected = false;
+    protected static final String DATA_KEY = "datas";
+
+    /**
+     * 静态工厂方法
+     * @param tClass
+     * @param <T>
+     * @return
+     */
+    public static <T extends BaseFragment> T getInstance(Class<T> tClass){
+        T t = null;
+        try {
+            t = tClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+    /**
+     * 初始化执行方法
+     * @param view
+     */
+    protected void init(View view){
+
+    }
+
+    /**
+     * 加载数据 -- 如果需要加载数据，重写该方法
+     */
+    public void loadDatas(){
+
+    }
+
+    /**
+     * 需要传递实体参数给fragment时调用该方法
+     * @param ks
+     * @return
+     */
+    public BaseFragment bindDatas(K ks){
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(DATA_KEY, ks);
+        this.setArguments(bundle);
+        return this;
+    }
+
+    /**
+     * 需要传递多个参数给fragment时调用该方法
+     * @return
+     */
+    public BaseFragment bindDatas(Object... obj){
+        Bundle bundle = setDatas(obj);
+        this.setArguments(bundle);
+        return this;
+    }
+
+    /**
+     * 多数据的处理方法 -- 通过bindDatas(Object... obj)方式传值时需要重写这个方法
+     * @param obj
+     * @return
+     */
+    protected Bundle setDatas(Object... obj){
+        return null;
+    }
+
+    /**
+     * 获得数据方法 -- 通过bindDatas(Object... obj)方式传值时需要重写这个方法
+     */
+    protected void getDatas(Bundle bundle){
+        if(bundle != null) {
+            K ks = (K) bundle.getSerializable(DATA_KEY);
+            if (ks != null) {
+                getDatas(ks);
+            }
+        }
+    }
+
+    /**
+     * 获得实体数据方法 -- 通过bindDatas(K ks)方式传值时需要重写这个方法
+     */
+    protected void getDatas(K ks){
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        injected = true;
+        //注册xutils
+        return x.view().inject(this, inflater, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (!injected) {
+            x.view().inject(this, this.getView());
+        }
+        init(view);
+        loadDatas();
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getDatas(getArguments());
+    }
+
+
+    /**
+     * fragment管理方法
+     */
+    public void fragmentManager(int fl_resid, Class fclass){
+        Activity activity = getActivity();
+        if(activity instanceof BaseActivity){
+            BaseActivity baseActivity = (BaseActivity) activity;
+            baseActivity.fragmentManager(fl_resid, fclass);
+        }
+    }
+}
