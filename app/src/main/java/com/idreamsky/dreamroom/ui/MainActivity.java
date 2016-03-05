0935e2591883ae2f32905c9e036cf758fefaf9aa
magical-xu <zx18507008756@163.com>
2016-03-05 15:55:17 +0800

    2

--- app/src/main/java/com/idreamsky/dreamroom/ui/MainActivity.java
+++ app/src/main/java/com/idreamsky/dreamroom/ui/MainActivity.java
@@ -1,18 +1,13 @@
 package com.idreamsky.dreamroom.ui;
 
-import android.widget.TextView;
-
 import com.idreamsky.dreamroom.R;
 import com.idreamsky.dreamroom.base.BaseActivity;
 
 import org.xutils.view.annotation.ContentView;
-import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
-    @ViewInject(R.id.text)
-    private TextView textView;
 
     @Override
     public void init() {
@@ -22,6 +17,5 @@ public class MainActivity extends BaseActivity {
     @Override
     public void loadDatas() {
 
-        textView.setText("xUtil真是太好用了");
     }
 }
