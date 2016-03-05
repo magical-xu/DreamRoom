bdd53f785f7c62257d3b2a784f8d4c59e98f54db
magical-xu <zx18507008756@163.com>
2016-03-05 16:23:40 +0800

    4

--- app/src/main/java/com/idreamsky/dreamroom/ui/MainActivity.java
+++ app/src/main/java/com/idreamsky/dreamroom/ui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.idreamsky.dreamroom.ui;
 
+import android.support.v7.widget.RecyclerView;
+
 import com.idreamsky.dreamroom.R;
 import com.idreamsky.dreamroom.base.BaseActivity;
 
@@ -8,6 +10,7 @@ import org.xutils.view.annotation.ContentView;
 @ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
+    private RecyclerView mRecyclerView;
 
     @Override
     public void init() {
